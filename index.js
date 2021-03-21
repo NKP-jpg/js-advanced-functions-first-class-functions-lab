c15597b8f5a0c7a72b66debac61d8a401e58a976
Nija <nija.perry21@gmail.com>
2021-03-20 21:22:29 -0400

    commit tree 2dce8fae6001c71ed867c4324c5ee00d7282e905

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (arr) {
+  return arr.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (arr) {
+  return arr.slice(2, 4);
+}
+
+
+const selectingDrivers = (returnFirstTwoDrivers) => {
+  return returnFirstTwoDrivers();
+}
+
+
+// (['Sally', 'Bob', 'Freddy', 'Claudia']);
+
+// function fun1() { 
+//   function fun2() { 
+//       return "From function fun2"; 
+//   } 
+//   return fun2(); 
+// } 
